e6beae693f52b582173bd7d9078a4bbe2ff7b999
Jonathan Stacks <jonstacks13@gmail.com>
2022-12-17 16:44:33 -0600

    Trigger update when annotations change

--- internal/controllers/tunnel_controller.go
+++ internal/controllers/tunnel_controller.go
@@ -96,6 +96,13 @@ func (trec *TunnelReconciler) SetupWithManager(mgr ctrl.Manager) error {
 			return false
 		},
 		UpdateFunc: func(ue event.UpdateEvent) bool {
+			// First check if there are any annotations present that aren't in the old version
+			oldAnnotations := ue.ObjectOld.GetAnnotations()
+			for newKey, newValue := range ue.ObjectNew.GetAnnotations() {
+				if oldAnnotations[newKey] != newValue {
+					return true
+				}
+			}
 			// No change to spec, so we can ignore.  This does not filter out updates
 			// that set metadata.deletionTimestamp, so this won't undermine finalizer.
 			return ue.ObjectNew.GetGeneration() != ue.ObjectOld.GetGeneration()
